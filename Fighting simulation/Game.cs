0c25751a2f345190c3005ad28d6b4eb995c07f16
jasper <s218005@students.aie.edu>
2021-08-25 16:58:08 -0500

    created first monster fight and second

---
+++ Fighting simulation/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fighting_simulation
+{
+    class Game
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
